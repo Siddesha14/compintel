8f6c824fba67fe34c7821d760df57346cf3680cf
Siddesh M <siddeshsiddu1299@gmail.com>
2026-05-28 14:16:59 +0530

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client/edge";
+const { PrismaClient } = require("@prisma/client");
 import { neon } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 
@@ -7,7 +7,7 @@ const globalForPrisma = globalThis as unknown as { prisma: any };
 function createPrismaClient() {
   const neonClient = neon(process.env.DATABASE_URL!);
   const adapter = new PrismaNeon(neonClient);
-  return new PrismaClient({ adapter } as any);
+  return new PrismaClient({ adapter });
 }
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient();
