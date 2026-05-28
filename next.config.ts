1d25f30794294cc944804ea51e21c5833473fa7a
Siddesh M <siddeshsiddu1299@gmail.com>
2026-05-28 14:09:21 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["pg", "@prisma/adapter-pg", "@prisma/client", "@neondatabase/serverless"],
-  experimental: {
-    turbo: {
-      resolveExtensions: [".ts", ".tsx", ".js", ".jsx"],
-    },
-  },
 };
 
 export default nextConfig;
